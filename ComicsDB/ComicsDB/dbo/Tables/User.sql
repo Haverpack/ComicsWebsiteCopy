4fbb087cca373fc03a4bd9aa6aa003fbf37fac7a
Mohammad Fahd <mohammad.fahd@ucalgary.ca>
2022-04-05 22:53:35 -0600

    Attempted to add DB (just a test as of now so it is incomplete)

---
+++ ComicsDB/ComicsDB/dbo/Tables/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User] (
+    [userID]   INT        NOT NULL,
+    [email]    NCHAR (10) NULL,
+    [userName] NCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([userID] ASC)
+);
+
